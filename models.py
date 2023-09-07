98874ff811949eade2c0fe0428f48cf8b690ac9c
samanmalekian <malekian@protonmail.com>
2023-09-08 01:07:40 +0330

    - Merged all historical tables to one table called CurrenciesHistory.

--- models.py
+++ models.py
@@ -3,11 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import (
     create_engine,
     Column,
-    Boolean,
     String,
-    DateTime,
     Integer,
     ForeignKey,
+    TIMESTAMP,
+    DateTime
 )
 
 DB_STRING = "postgresql://postgres:1234@localhost:5432/coinmarketcap_db"
@@ -34,52 +34,22 @@ class Currency(base):
     tags = relationship("Tag")
 
 
-class Date(base):
-    __tablename__ = "date"
+class CurrenciesHistory(base):
+    __tablename__ = "currencies_history"
 
     id = Column(Integer, unique=True, primary_key=True, autoincrement=True)
-    date = Column(DateTime)
-
-    # Relationships
-    currency_date = relationship("CurrencyDate")
-
-
-class Market(base):
-    __tablename__ = "market"
-
-    id = Column(Integer, unique=True, primary_key=True, autoincrement=True)
-    marketCap = Column(Integer)
-    currency_date = Column(Integer, ForeignKey("currency_date.id"))
-
-
-class Trade(base):
-    __tablename__ = "trade"
-
-    id = Column(Integer, unique=True, primary_key=True, autoincrement=True)
-    volume = Column(Integer)
-    currency_date = Column(Integer, ForeignKey("currency_date.id"))
-
-
-class Price(base):
-    __tablename__ = "price"
-
-    id = Column(Integer, unique=True, primary_key=True, autoincrement=True)
+    timeOpen = Column(DateTime)
+    timeClose = Column(DateTime)
+    timeHigh = Column(DateTime)
+    timeLow = Column(DateTime)
     open = Column(Integer)
-    close = Column(Integer)
     high = Column(Integer)
     low = Column(Integer)
-    currency_date = Column(Integer, ForeignKey("currency_date.id"))
-
-
-class Time(base):
-    __tablename__ = "time"
-
-    id = Column(Integer, unique=True, primary_key=True, autoincrement=True)
-    timeOpen = Column(Integer)
-    timeClose = Column(Integer)
-    timeHigh = Column(Integer)
-    timeLow = Column(Integer)
-    currency_date = Column(Integer, ForeignKey("currency_date.id"))
+    close = Column(Integer)
+    volume = Column(Integer)
+    marketCap = Column(Integer)
+    timestamp = Column(TIMESTAMP)
+    currency = Column(Integer, ForeignKey("currency.id"))
 
 
 class Tag(base):
@@ -88,21 +58,6 @@ class Tag(base):
     id = Column(Integer, unique=True, primary_key=True, autoincrement=True)
     tag = Column(String)
     currency = Column(Integer, ForeignKey("currency.id"))
-
-
-class CurrencyDate(base):
-    __tablename__ = "currency_date"
-
-    id = Column(Integer, unique=True, primary_key=True, autoincrement=True)
-
-    # Relationships:
-    currency = Column(Integer, ForeignKey("currency.id"))
-    date = Column(Integer, ForeignKey("date.id"))
-
-    market = relationship("Market")
-    trade = relationship("Trade")
-    price = relationship("Price")
-    time = relationship("Time")
 
 
 if __name__ == "__main__":
